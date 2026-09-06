830025fa205b23ccd36e29b74030407eb59687c4
Grigio31 <grigio@gmail.com>
2026-09-06 19:07:08 +0200

    Add SQdecode64 VST3 identifiers

---
+++ SQdecode64/vst3/SQdecodeIDs.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "pluginterfaces/base/funknown.h"
+namespace sqdecode64::vst3 {
+static const Steinberg::FUID kProcessorUID (0x5D11F447,0xE7734A88,0xA1C58CC5,0x650CB103);
+static const Steinberg::FUID kControllerUID(0x30C42792,0x71024F09,0x87EBB9B6,0x31047E12);
+}
